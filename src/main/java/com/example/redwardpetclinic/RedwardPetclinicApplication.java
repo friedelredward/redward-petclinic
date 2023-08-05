72d95d47dc6bd93b49c45919e0657c2233d5437f
R Edward Friedel <redwardweb@gmail.com>
2023-08-05 21:05:25 +0200

    initial commit petclinic

---
+++ src/main/java/com/example/redwardpetclinic/RedwardPetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.redwardpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedwardPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedwardPetclinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/redwardpetclinic/RedwardPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redwardpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedwardPetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
