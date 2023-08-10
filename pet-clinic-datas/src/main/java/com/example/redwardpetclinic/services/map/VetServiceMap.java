1e32135194f61b500c4ef742bafb5ed26b7ddbc7
R Edward Friedel <redwardweb@gmail.com>
2023-08-10 15:55:31 +0200

    add: added spring IoC for dataloader + annotations. Please note that data module lives under the same package that's why spring can see it, and no need for component scan or importModule  #17.
    Signed-off-by: R Edward Friedel <redwardweb@gmail.com>

--- pet-clinic-datas/src/main/java/com/example/redwardpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-datas/src/main/java/com/example/redwardpetclinic/services/map/VetServiceMap.java
@@ -2,12 +2,14 @@ package com.example.redwardpetclinic.services.map;
 
 import com.example.redwardpetclinic.model.Vet;
 import com.example.redwardpetclinic.services.VetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 /**
  * Created by r.edward on {08/08/2023}
  */
+@Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
--- pet-clinic-web/src/main/java/com/example/redwardpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/example/redwardpetclinic/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import com.example.redwardpetclinic.model.Owner;
 import com.example.redwardpetclinic.model.Vet;
 import com.example.redwardpetclinic.services.OwnerService;
 import com.example.redwardpetclinic.services.VetService;
-import com.example.redwardpetclinic.services.map.OwnerServiceMap;
-import com.example.redwardpetclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,9 +15,17 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
+/*lets inject this the spring way!!
+remember to annotate with serve the implementation ...ServiceMap
+
     public DataLoader() {
         ownerService = new OwnerServiceMap();
         vetService = new VetServiceMap();
+    }*/
+
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
     @Override
