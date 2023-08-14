9d0507d6fcb4b5e058dda8b9f421e89ce2d83f17
R Edward Friedel <redwardweb@gmail.com>
2023-08-14 22:27:57 +0200

    add: contact info to owner. Closes #40.
    Signed-off-by: R Edward Friedel <redwardweb@gmail.com>

--- pet-clinic-datas/src/main/java/com/example/redwardpetclinic/model/Owner.java
+++ pet-clinic-datas/src/main/java/com/example/redwardpetclinic/model/Owner.java
@@ -6,7 +6,10 @@ import java.util.Set;
  * Created by r.edward on {07/08/2023}
  */
 public class Owner extends Person{
-    Set<Pet> pets;
+    private Set<Pet> pets;
+    private String address;
+    private String city;
+    private String telefone;
 
     public Set<Pet> getPets() {
         return pets;
@@ -14,5 +17,29 @@ public class Owner extends Person{
 
     public void setPets(Set<Pet> pets) {
         this.pets = pets;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
     }
 }
