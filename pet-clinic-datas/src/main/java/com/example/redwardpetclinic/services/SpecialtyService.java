2dd304fbdb2598cbc726d325763b09d2d86a3603
R Edward Friedel <redwardweb@gmail.com>
2023-08-15 00:05:12 +0200

    add: added specialtyy map service #42.
    Signed-off-by: R Edward Friedel <redwardweb@gmail.com>

---
+++ pet-clinic-datas/src/main/java/com/example/redwardpetclinic/services/SpecialtyService.java
@@ -0,0 +1,8 @@
+package com.example.redwardpetclinic.services;
+
+import com.example.redwardpetclinic.model.Specialty;
+
+/**
+ * Created by r.edward on {07/08/2023}
+ */
+public interface SpecialtyService extends CrudService<Specialty, Long> {}
---
+++ pet-clinic-datas/src/main/java/com/example/redwardpetclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,37 @@
+package com.example.redwardpetclinic.services.map;
+
+import com.example.redwardpetclinic.model.Specialty;
+import com.example.redwardpetclinic.services.SpecialtyService;
+
+import java.util.Set;
+
+/**
+ * Created by r.edward on {14/08/2023}
+ */
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}
