e13f10f0416b1981bf0e37142212d9cd6b076464
R Edward Friedel <redwardweb@gmail.com>
2023-08-15 00:15:06 +0200

    add: added specialtyy map service .Closes #45.
    Signed-off-by: R Edward Friedel <redwardweb@gmail.com>

--- pet-clinic-datas/src/main/java/com/example/redwardpetclinic/services/map/SpecialtyMapService.java
+++ pet-clinic-datas/src/main/java/com/example/redwardpetclinic/services/map/SpecialtyMapService.java
@@ -2,12 +2,14 @@ package com.example.redwardpetclinic.services.map;
 
 import com.example.redwardpetclinic.model.Specialty;
 import com.example.redwardpetclinic.services.SpecialtyService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 /**
  * Created by r.edward on {14/08/2023}
  */
+@Service
 public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
 
     @Override
--- pet-clinic-web/src/main/java/com/example/redwardpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/example/redwardpetclinic/bootstrap/DataLoader.java
@@ -1,11 +1,9 @@
 package com.example.redwardpetclinic.bootstrap;
 
-import com.example.redwardpetclinic.model.Owner;
-import com.example.redwardpetclinic.model.Pet;
-import com.example.redwardpetclinic.model.PetType;
-import com.example.redwardpetclinic.model.Vet;
+import com.example.redwardpetclinic.model.*;
 import com.example.redwardpetclinic.services.OwnerService;
 import com.example.redwardpetclinic.services.PetTypeService;
+import com.example.redwardpetclinic.services.SpecialtyService;
 import com.example.redwardpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -20,6 +18,7 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
     private final PetTypeService petTypeService;
+    private final SpecialtyService specialtyService;
 
 /*lets inject this the spring way!!
 remember to annotate with serve the implementation ...ServiceMap
@@ -29,14 +28,18 @@ remember to annotate with serve the implementation ...ServiceMap
         vetService = new VetServiceMap();
     }*/
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
+
+        this.specialtyService = specialtyService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        //we save these because we need them with id when we add them to
+//        bigger entity
         PetType petType= new PetType();
         petType.setName("dog");
         PetType savedPetType= petTypeService.save(petType);// we save ref for later
@@ -44,6 +47,16 @@ remember to annotate with serve the implementation ...ServiceMap
         PetType petTypeCat= new PetType();
         petType.setName("cat");
         PetType savedCatType= petTypeService.save(petTypeCat);
+
+        Specialty radiology= new Specialty();
+        radiology.setDescription("radiology");
+        Specialty savedRadiology= specialtyService.save(radiology);
+        Specialty dentistry= new Specialty();
+        radiology.setDescription("dentistry");
+        Specialty savedDentistry= specialtyService.save(dentistry);
+        Specialty surgery= new Specialty();
+        radiology.setDescription("surgery");
+        Specialty savedSurgery= specialtyService.save(surgery);
 
         Owner o1= new Owner();
         o1.setFirstName("bill");
@@ -83,6 +96,7 @@ remember to annotate with serve the implementation ...ServiceMap
         v1.setFirstName("Lucky");
         v1.setLastName("Friedel");
         v1.setId(1L);
+        v1.getSpecialties().add(savedRadiology);
         vetService.save(v1);
 
         Vet v2= new Vet();
@@ -90,7 +104,7 @@ remember to annotate with serve the implementation ...ServiceMap
         v2.setLastName("MaromoFamily");
         v2.setId(2L);
         vetService.save(v2);
-
+        v2.getSpecialties().add(savedSurgery);
         System.out.println("loaded Owners and Vets");
     }
 }
