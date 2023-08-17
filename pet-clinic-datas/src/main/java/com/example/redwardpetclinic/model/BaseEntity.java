136203392d08e68bbf096a2ba63780fbb62695f4
R Edward Friedel <redwardweb@gmail.com>
2023-08-17 13:54:51 +0200

    add: added JPA base class. Closes #46.
    Signed-off-by: R Edward Friedel <redwardweb@gmail.com>

--- pet-clinic-datas/src/main/java/com/example/redwardpetclinic/model/BaseEntity.java
+++ pet-clinic-datas/src/main/java/com/example/redwardpetclinic/model/BaseEntity.java
@@ -1,11 +1,19 @@
 package com.example.redwardpetclinic.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
 import java.io.Serializable;
 
 /**
  * Created by r.edward on {07/08/2023}
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
