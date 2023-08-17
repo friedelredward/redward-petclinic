21fe477e9ad471b5ab53d1bc9223ffdf8f272d62
R Edward Friedel <redwardweb@gmail.com>
2023-08-17 16:42:57 +0200

    add: added pet JPA service with profile for collision. Closes #59.
    Signed-off-by: R Edward Friedel <redwardweb@gmail.com>

---
+++ pet-clinic-datas/src/main/java/com/example/redwardpetclinic/services/springdatajpa/SpecialtyJPAService.java
@@ -0,0 +1,50 @@
+package com.example.redwardpetclinic.services.springdatajpa;
+
+import com.example.redwardpetclinic.model.Specialty;
+import com.example.redwardpetclinic.repositories.SpecialtyRepository;
+import com.example.redwardpetclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by r.edward on {17/08/2023}
+ */
+@Service
+@Profile("springdatajpa")
+public class SpecialtyJPAService implements SpecialtyService {
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtyJPAService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialties= new HashSet<>();
+        specialtyRepository.findAll().forEach(specialties::add);
+        return specialties;
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
